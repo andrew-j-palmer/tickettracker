8b4145882a87d07b00f070b46e98dfce16f6f48a
Andrew Palmer <andy@showcasetech.net>
2018-03-11 22:40:41 -0500

    trying new prod vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,14 +23,16 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #** TEMP DISABLE BECAUSE I THINK THIS IS CAAUSING PROBLEMS **
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #** ALSO TEMP CHANING THIS**
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
