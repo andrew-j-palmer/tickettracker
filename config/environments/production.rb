ea1c605d55dd1b82e13413dde54ba1681b472b20
Andrew Palmer <andrewjpalmer@mail.com>
2018-03-20 22:37:14 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   #** TEMP DISABLE BECAUSE I THINK THIS IS CAAUSING PROBLEMS **
   #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
